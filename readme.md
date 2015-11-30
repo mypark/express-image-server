85e02ebc4e127e55aa2dc64d59fa7a42b661bc37
Michael Park <mike@closerto.com>
2015-11-30 13:03:33 -0500

    changed from s3 to http get and cleaned up parameters

--- readme.md
+++ readme.md
@@ -1,30 +1,21 @@
 #Express Image Server
 
 ##Intro
-A simple Node.js image server built on express and ImageMagick suitable for hosting on ec2 or Heroku.  Photos are uploaded to an Amazon s3 bucket.  It is recommended that you also use a CDN and set this server as an origin point.
+A simple Node.js image server that resizes images fetch from a remote url, built on express and ImageMagick suitable for hosting on ec2 or Heroku.
+It is recommended that you also use a CDN and set this server as an origin point.
 
-##Configuration
+##Options
+These parameters to be passed in the querystring
 
-Configure the following environment variables
-
-* **S3_UPLOAD_BUCKET** - your source upload bucket where original images jpg or pngs are stored
-
-* **AWS_ACCESS_KEY_ID** – AWS access key.
-
-* **AWS_SECRET_ACCESS_KEY** – AWS secret key. Access and secret key variables override credentials stored in credential and config files.
-
-* **AWS_SESSION_TOKEN** – session token. A session token is only required if you are using temporary security credentials.
-
-* **AWS_DEFAULT_REGION** – AWS region. This variable overrides the default region of the in-use profile, if set.
+size: Size is the max pixels to resize the original image to, and the dimension varies by mode.
+mode: "crop" and "pad" create square thumbnails, while "width" and "height" will set that dimension as the longest.  Defaults to no mode which will resize to longest dimension and maintain aspect ratio.
+pixelate: defaults to false, pixelates the image.
 
 Example Usage
-http://yourserver/photo/{filename}?size=300
+http://yourserver/photos/resize/?url={photoUrl}&size=300&mode=crop&pixelate=true
 
-Size is the max pixels to resize the original image to.  It resizes to the longest dimension.  
 
-Filename is the original filename in the upload bucket on s3.
 
 ##Todos
-1. add the upload service that creates a temporary s3 upload token to upload the photo and provide an upload example.
-2. Make this a public npm package.
-3. Make startup code simpler and remove auto-generated express/www code
+1. Make this a public npm package.
+2. Make startup code simpler and remove auto-generated express/www code
--- routes/photos.js
+++ routes/photos.js
@@ -1,143 +1,101 @@
 var express = require('express');
 var router = express.Router();
-
-var AWS = require('aws-sdk');
+var request = require('request');
 var gm = require('gm')
     .subClass({imageMagick: true}); // Enable ImageMagick integration.
-
 var imageType = require('image-type');
 
-var s3 = new AWS.S3();
-
-var srcBucket = process.env.S3_UPLOAD_BUCKET;
+router.get('/resize', function (req, res, next) {
 
 
-/* GET photo from s3 */
-router.get('/:key', function (req, res, next) {
+    //var srcKey = req.params.key;
+    var imageUrl = req.query.src;
+    var mode = req.query.mode;
+    var max_size = req.query.size ? parseInt(req.query.size) : 200;
+    var pixelate = req.query.pixelate || false;
 
-
-    var srcKey = req.params.key;
-
-    var targetSize = {
-        dimension:null,
-        size:100
+    var options = {
+        mode: mode,
+        size: max_size,
+        pixelate: pixelate
     };
 
-    var width = req.query.w;
-    var height = req.query.h;
-    var max_size = req.query.size || 200;
+    request({method:'GET', url:imageUrl, encoding:null}, function(error, response, body) {
 
-    if (width) {
-        targetSize.dimension = 'w';
-        targetSize.size = width;
-    } else if (height) {
-        targetSize.dimension = 'h';
-        targetSize.size = height;
-    } else {
-        targetSize.dimension = null;
-        targetSize.size = max_size;
-    }
-
-    // Download the image from S3
-    s3.getObject({
-        Bucket: srcBucket,
-        Key: srcKey
-    }, function (err, response) {
-        if (err) {
+        if (error) {
             console.error('unable to download image ' + err);
             return res.status(404).send('unable to download image ' + err);
         }
 
-        if (!response.Body)
+        if (!body)
             return res.status(404).send('no body response for image');
 
-        var imgType = imageType(response.Body)
+        var imgType = imageType(body);
 
         if (!imgType || (imgType.mime != "image/jpeg" && imgType.mime != "image/png")) {
             console.log('skipping non-image ' + srcKey);
             return res.status(400).send('not an image type');
         }
-
-        var original = gm(response.Body);
-
-        if (req.query.pixelate)
-        {
-            pixelated_thumb(original, max_size, function(err, photo){
-                res.setHeader('Content-Type', 'image/png');
+        var original = gm(body);
+        original.size(function (err, size) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send(err);
+            }
+            resize_photo(size, options, original, function (err, photo) {
+                res.setHeader('Content-Type', 'image/jpeg');
                 res.send(photo);
             });
-
-        } else {
-            original.size(function (err, size) {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).send(err);
-                }
-
-                resize_photo(size, targetSize, original, function (err, photo) {
-                    res.setHeader('Content-Type', 'image/jpeg');
-                    res.send(photo);
-                });
-            });
-        }
-    });
-
-    //res.send('respond with a resource');
-});
-
-var pixelated_thumb = function(original, size, next){
-
-    original
-        .autoOrient()
-        .thumbnail(size, size + '^')
-        .gravity('Center')
-        .extent(size,size)
-        .scale('10%').scale('1000%')
-        .toBuffer('png', function (err, buffer) {
-            if (err) {
-                next(err);
-            }
-            else {
-                next(null, buffer);
-            }
         });
 
-}
+    });
 
-//wrap up variables into an options object
-var resize_photo = function (size, max_size, original, next) {
+});
+
+var resize_photo = function (size, options, original, next) {
     
     // Infer the scaling factor to avoid stretching the image unnaturally.
     var scalingFactor;
 
-    switch(max_size.dimension) {
-        case 'w':
-            scalingFactor = max_size.size / size.width;
+    switch(options.mode) {
+        case 'width':
+            scalingFactor = options.size / size.width;
             break;
-        case 'h':
-            scalingFactor =  max_size.size / size.height;
+        case 'height':
+            scalingFactor =  options.size / size.height;
+            break;
+        case 'crop':
+            scalingFactor = Math.max(options.size / size.width, options.size / size.height); //for cropping to square
             break;
         default:
-            scalingFactor = Math.min(max_size.size / size.width, max_size.size / size.height);
+            scalingFactor = Math.min(options.size / size.width, options.size / size.height); //for either size
     }
 
     var width = scalingFactor * size.width;
     var height = scalingFactor * size.height;
 
-    // Transform the image buffer in memory.
-    original.resize(width, height).autoOrient()
-        .toBuffer('jpg', function (err, buffer) {
-            if (err) {
-                next(err);
-            }
-            else {
-                next(null, buffer);
-            }
-        });
+    var original = original
+        .resize(width, height)
+        .autoOrient();
+
+    if (options.mode === 'pad' || options.mode === 'crop') {
+        original.gravity('Center').extent(options.size, options.size);
+    }
+
+    if (options.pixelate) {
+        original.scale('10%').scale('1000%')
+    }
+
+    original.toBuffer('jpg', function (err, buffer) {
+        if (err) {
+            next(err);
+        }
+        else {
+            next(null, buffer);
+        }
+    });
 
 
 };
-
-
 
 module.exports = router;
